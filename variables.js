626b5f88ce3c539e3db18fbe10d4113e085dd91a
Miguel Ardila <miguel_sxe@hotmail.com>
2023-09-19 11:09:14 -0500

    variables y console.log

---
+++ variables.js
@@ -0,0 +1,32 @@
+// En JavaScript, las variables son contenedores que se utilizan para almacenar datos, como números, cadenas de texto, objetos, funciones y otros tipos de valores.
+
+// polyfill Es un codigo que provee el fucnionamiento de una nueva caracteristica de javascript (ES6) en versiones viejas como ES5
+
+let a = 10;
+let b = 10;
+let c = 'hello ';
+let d = 'wordl';
+let x =  a + b;
+
+const saludo = c + d; // para que se imprima saludo se debe escribir saludo en consola.
+
+console.log(x);
+
+ // otro ejmplo
+let i = 23, f = 10, j = 44, z = i + j;
+console.log('z', z);
+console.log('i', i);
+console.log('f', f);
+
+// console.log('f', f);  ('f',f); es un ejemplo que imprimaa en la consola z 67 i 23 f 10
+// clg para escirbir el codigo rapido de console.log
+
+console.log({z});
+console.log({i});
+console.log({f});
+
+// imprime todo en una tabla en consola
+console.table({a, b, c, d, x});
+
+
+
