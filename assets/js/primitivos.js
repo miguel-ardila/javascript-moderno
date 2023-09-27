c751f902e9a1b4337ce6e68923a0891b9d2a84c5
Miguel Ardila <miguel_sxe@hotmail.com>
2023-09-26 19:19:52 -0500

    practica primitivos

---
+++ assets/js/primitivos.js
@@ -0,0 +1,39 @@
+
+// Tipo de datos string
+let nombre = "Amy";
+console.log(nombre);
+
+nombre = "Amy Ardila Barrera";
+console.log(nombre);
+
+nombre = "Stefanny";
+nombre = `stefanny`;
+console.log(typeof nombre);
+
+// Tipo de datos boolean
+let esFamilia = false;
+console.log(typeof esFamilia);
+
+// Tipo de datos numeros
+
+let edad = 31; // numero entero 
+console.log(typeof edad);
+
+edad = 33.1 // numero flotante
+console.log(typeof edad);
+
+// Tipo de datos undefined
+
+let superFamilia;
+console.log(typeof superFamilia);
+
+// Tipo de datos null
+
+let soyNull = null;
+console.log(typeof soyNull);
+
+// Tipo de datos symbol
+
+let symbol1 = Symbol ('a');
+let symbol2 = Symbol ('b');
+console.log(typeof symbol1);
