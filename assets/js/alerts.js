9d514381e7f3749d5cc53559aefc2025a423df96
Miguel Ardila <miguel_sxe@hotmail.com>
2023-09-26 19:19:14 -0500

    Prompt, confirm y alert

---
+++ assets/js/alerts.js
@@ -0,0 +1,16 @@
+// alert es una función incorporada que se utiliza para mostrar un cuadro de diálogo de alerta en un navegador web
+
+// alert("Hola mundo!");
+
+
+//  prompt función incorporada que se utiliza para mostrar un cuadro de diálogo que permite al usuario ingresar un valor o una respuesta. La función prompt toma dos argumentos: el primer argumento es el mensaje que se muestra al usuario y el segundo argumento es el valor predeterminado que aparecerá en el campo de entrada del cuadro de diálogo.
+
+// let nombre = prompt("cual es tu nombre");
+// console.log(nombre);
+// console.log("****" + nombre + "****"); // "" string vacio
+
+
+// confirm es otra función incorporada que se utiliza para mostrar un cuadro de diálogo de confirmación en un navegador web. A diferencia de alert, el cuadro de diálogo de confirmación tiene dos opciones para el usuario: "Aceptar" y "Cancelar". Esta función se utiliza para obtener una respuesta binaria del usuario, generalmente para confirmar o cancelar una acción.
+
+const seleccion = confirm("Estas seguro de Borrar?");
+console.log(seleccion);
