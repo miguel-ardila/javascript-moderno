f9b694d1a58d9b23b76ba0a11102d596afbd1bde
Miguel Ardila <miguel_sxe@hotmail.com>
2023-10-10 19:32:51 -0500

    practica array

--- assets/js/arrays.js
+++ assets/js/arrays.js
@@ -3,3 +3,22 @@
 const arr = new Array(10);
 console.log(arr);
 
+
+let videojuegos = ["Aped-legends", "valorant", "Halo", "Fornite"];
+console.log({videojuegos});
+console.log(videojuegos); //imprime los arrays completos en consola
+console.log(videojuegos [1]); //imprime consola la posicion 1 del array video juegos en ese caso seria valorant
+
+let arregloCosas = [
+    true,
+    123,
+    "Amy",
+    1 + 2,
+    function(){},
+    ()=>{},
+    {a: 1 },
+    ["x", "Megaman", "zero"]
+];
+console.log(arregloCosas);
+console.log({arregloCosas});
+console.log(arregloCosas[7][2]); //array dentro de otro array
